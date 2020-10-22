fa389f13d1419eadf1cd540212b69c432d0be6ae
Robert <bobbierob610@gmail.com>
2020-10-22 18:42:12 -0400

    worked on main, got cat to move via arrow keys

--- main.py
+++ main.py
@@ -3,40 +3,63 @@ from pygame.locals import *
 
 pygame.init()
 
-FPS = 144 # frames per second setting
+FPS = 60 # frames per second setting
 fpsClock = pygame.time.Clock()
 
 # set up the window
-DISPLAYSURF = pygame.display.set_mode((400, 300), 0, 32)
+WINDOWWIDTH = 800
+WINDOWHEIGHT = 600
+DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT), 0, 32)
 pygame.display.set_caption('Animation')
 WHITE = (255, 255, 255)
-catImg = pygame.image.load('cat.png')
-catx = 10
-caty = 10
-direction = 'right'
 
-while True: # the main game loop
-    DISPLAYSURF.fill(WHITE)
-    if direction == 'right':
-        catx += 1
-        if catx == 280:
-            direction = 'down'
-    elif direction == 'down':
-        caty += 1
-        if caty == 220:
-            direction = 'left'
-    elif direction == 'left':
-        catx -= 1
-        if catx == 10:
-            direction = 'up'
-    elif direction == 'up':
-        caty -= 1
-        if caty == 10:
-            direction = 'right'
-    DISPLAYSURF.blit(catImg, (catx, caty))
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-    pygame.display.update()
-    fpsClock.tick(FPS)
+catImg = pygame.image.load('cat.png')
+catImgX, catImgY = catImg.get_rect().size
+catx = WINDOWWIDTH / 2 - (catImgX / 2)
+caty = WINDOWHEIGHT - catImgY
+CATSPEED = 5
+
+def main():
+    while True: # the main game loop
+        DISPLAYSURF.fill(WHITE)
+        # keep cat in frame
+        if catx < 0:
+            catx = 0
+        if catx > WINDOWWIDTH - catImgX:
+            catx = WINDOWWIDTH - catImgX
+        if caty < 0:
+            caty = 0
+        if caty > WINDOWHEIGHT - catImgY:
+            caty = WINDOWHEIGHT - catImgY
+
+        DISPLAYSURF.blit(catImg, (catx, caty))
+        keystate = pygame.key.get_pressed()
+        if keystate[K_UP]:
+            caty -= CATSPEED
+        if keystate[K_DOWN]:
+            caty += CATSPEED
+        if keystate[K_LEFT]:
+            catx -= CATSPEED
+        if keystate[K_RIGHT]:
+            catx += CATSPEED
+
+        for event in pygame.event.get():
+            print(event)
+            if event.type == KEYDOWN:
+                if event.key == K_SPACE:
+
+                if event.key == K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+        pygame.display.update()
+        fpsClock.tick(FPS)
+
+def Jump(image, height):
+    pass
+
+if __name__ == '__main__':
+    main()
